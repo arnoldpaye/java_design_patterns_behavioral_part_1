11452ac860307af8173cb12b1dc82a0879a7c83b
Arnold Paye <arnoldpayet@gmail.com>
2022-09-05 08:02:09 -0400

    add chain_of_responsibility_exercise package

---
+++ src/chain_of_responsibility_exercise/BasicAuthenticationHandler.java
@@ -0,0 +1,8 @@
+package chain_of_responsibility_exercise;
+
+public class BasicAuthenticationHandler extends AuthenticationHandler {
+
+  public void handleRequest(String requestType) {
+  }
+
+}
---
+++ src/chain_of_responsibility_exercise/Client.java
@@ -0,0 +1,9 @@
+package chain_of_responsibility_exercise;
+
+public class Client {
+
+  public static void main(String[] args) {
+
+  }
+
+}
