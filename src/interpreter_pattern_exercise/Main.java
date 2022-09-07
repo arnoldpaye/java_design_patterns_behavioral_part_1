f86d0d66a5f7df9b23f30da7a2b5d4804f021eb1
Arnold Paye <arnoldpayet@gmail.com>
2022-09-07 07:23:38 -0400

    add interpreter_pattern_exercise package

---
+++ src/interpreter_pattern_exercise/Main.java
@@ -0,0 +1,15 @@
+package interpreter_pattern_exercise;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    String context = "this is a a sentence";
+
+    // Interpret the sentence here
+
+    System.out.println(context);
+
+  }
+
+}
