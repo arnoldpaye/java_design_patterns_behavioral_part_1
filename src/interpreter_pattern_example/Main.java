5f4c6671e50d00d17b4ab8b3a3efcad71d9db9dc
Arnold Paye <arnoldpayet@gmail.com>
2022-09-07 07:21:14 -0400

    interpreting a variable

---
+++ src/interpreter_pattern_example/Main.java
@@ -0,0 +1,10 @@
+package interpreter_pattern_example;
+
+public class Main {
+  public static void main(String[] args) {
+    String context = "_Int";
+    FirstLetterNotUnderscore firstLetterNotUnderscore = new FirstLetterNotUnderscore();
+    context = firstLetterNotUnderscore.interpret(context);
+    System.out.println(context);
+  }
+}
