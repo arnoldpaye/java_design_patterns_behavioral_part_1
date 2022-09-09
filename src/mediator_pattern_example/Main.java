5de5d5e4dc22b142b89cdd4e43e30d4ee6d9bdab
Arnold Paye <arnoldpayet@gmail.com>
2022-09-09 06:44:33 -0400

    avoiding tight coupling between objects

---
+++ src/mediator_pattern_example/Main.java
@@ -0,0 +1,8 @@
+package mediator_pattern_example;
+
+public class Main {
+  public static void main(String[] args) {
+    Customer customer = new Customer("123 Sunny Street");
+    customer.buy("pens", 3);
+  }
+}
