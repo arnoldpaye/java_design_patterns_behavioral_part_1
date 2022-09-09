56715a3ccb6a0f8c5e385b8c5f92d9f9a0d2adc3
Arnold Paye <arnoldpayet@gmail.com>
2022-09-09 06:51:37 -0400

    add mediator_pattern_exercise package

---
+++ src/mediator_pattern_exercise/Main.java
@@ -0,0 +1,11 @@
+package mediator_pattern_exercise;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Plane plane = new Plane(123);
+    plane.takeOff();
+
+  }
+
+}
---
+++ src/mediator_pattern_exercise/Runway.java
@@ -0,0 +1,15 @@
+package mediator_pattern_exercise;
+
+public class Runway {
+
+  private boolean isAvailable = true;
+
+  public void setIsAvailable(boolean isAvailable) {
+    this.isAvailable = isAvailable;
+  }
+
+  public boolean getIsAvailable() {
+    return isAvailable;
+  }
+
+}
